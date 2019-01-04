7c595f4defdd4398dc262ed16f12211e6e35bf5b
Susant Sahani <ssahani@gmail.com>
2019-01-04 16:02:10 +0530

    Do not panic if can't retrieve FD from systemd socket activation

--- cmd/router/router.go
+++ cmd/router/router.go
@@ -63,11 +63,7 @@ func StartRouter(ip string, port string, tlsCertPath string, tlsKeyPath string) 
 	// socket activation
 	listeners, err := activation.Listeners()
 	if err != nil {
-		log.Panicf("cannot retrieve listeners: %s", err)
-	}
-
-	if len(listeners) != 1 {
-		log.Panicf("unexpected number of socket activation (%d != 1)", len(listeners))
+		log.Infof("Failed to retrieve listeners: %s", err)
 	}
 
 	if share.PathExists(tlsCertPath) && share.PathExists(tlsKeyPath) {
